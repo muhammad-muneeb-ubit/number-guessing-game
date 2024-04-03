4b406eca3157772410c04d3ee8cdb33f6d974a61
muhammad-muneeb-ubit <mohammadmunib1233@gmail.com>
2024-04-03 17:25:42 +0500

    number guessing game's files are added.

---
+++ main.js
@@ -0,0 +1,14 @@
+import inquirer from "inquirer";
+let randomNumber = Math.floor(Math.random() * 10 + 1);
+// console.log(randomNumber);
+let userInput = await inquirer.prompt([{
+        name: "userGuess",
+        message: "Enter your Number: ",
+        type: "String"
+    }]);
+if (userInput.userGuess == randomNumber) {
+    console.log("Congratulates! you won.");
+}
+else {
+    console.log("You guessed wrong number.");
+}
